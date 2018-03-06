6f8276de549474a806a3a1aed612d685a5fae419
Lance Good <lgoodyt@gmail.com>
2018-03-05 21:59:18 -0500

    Add files via upload

--- client.lua
+++ client.lua
@@ -8,21 +8,28 @@
 ---------------------------------- DON'T EDIT THESE LINES --------------------------------------
 ------------------------------------------------------------------------------------------------
 local cooldown = 0
+
 local ispriority = false
-  
--- Mission Text
+
+Blips = {}
+
 RegisterNetEvent("mt:missiontext")
-AddEventHandler("mt:missiontext", function(text, time)
-    ClearPrints()
-    SetTextEntry_2("STRING")
-    AddTextComponentString(text)
-    DrawSubtitleTimed(time, 1)
+AddEventHandler("mt:missiontext", function(mission, time)
+    for i=1, #Config.Missions[mission].MissionMessage do
+        text = Config.Missions[mission].MissionMessage
+        ClearPrints()
+        SetTextEntry_2("STRING")
+        AddTextComponentString(text)
+        DrawSubtitleTimed(time, 1)
+    end
 end)
 
--- Reset Clock
-RegisterCommand("cancelmission", function()
+RegisterCommand("stop", function(source, args, rawCommand)
+    mission = args[1]
+    TriggerEvent('chatMessage', "^1[MISSIONS]: ^2".. mission .."^0 has been completed! Great work.")
     TriggerServerEvent("cancelcooldown")
-    TriggerEvent("KP")
+    TriggerEvent("DONE", mission)
+    TriggerEvent("DELBlips")
 end, false)
 
 RegisterNetEvent('UpdateCooldown')
@@ -36,12 +43,61 @@ AddEventHandler('UpdateTimer', function(newispriority)
 end)
 
 RegisterNetEvent('missionBlips')
-AddEventHandler('missionBlips', function()
-    x = -187.2
-    y = -1091.96
-    z = 27.16
-    SetNewWaypoint(x, y)
+AddEventHandler('missionBlips', function(mission)
+    local blip = AddBlipForCoord(Config.Missions[mission].Blip.Position.x, Config.Missions[mission].Blip.Position.y, Config.Missions[mission].Blip.Position.z)
+    SetBlipSprite (blip, Config.Missions[mission].Blip.Icon)
+    SetBlipDisplay(blip, Config.Missions[mission].Blip.Display)
+    SetBlipScale  (blip, Config.Missions[mission].Blip.Size)
+    SetBlipColour (blip, Config.Missions[mission].Blip.Color)
+    SetBlipAsShortRange(blip, false)
+    local btitle = Config.Missions[mission].Blip.Title
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString(btitle)
+    EndTextCommandSetBlipName(blip)
+    table.insert(Blips, blip)
 end)
+
+RegisterNetEvent('DONE')
+AddEventHandler('DONE', function(mission)
+    for i=1, #Config.Missions[mission].Peds do
+        SetEntityHealth(Config.Missions[mission].Peds[i].id, 0)
+    end
+    for i, blip in pairs(Blips) do
+		RemoveBlip(blip)
+	end
+end)
+
+RegisterNetEvent('SpawnPed')
+AddEventHandler('SpawnPed', function(mission)
+    for i=1, #Config.Missions[mission].Peds do
+        RequestModel(GetHashKey(Config.Missions[mission].Peds[i].modelHash))
+    while not HasModelLoaded(GetHashKey(Config.Missions[mission].Peds[i].modelHash)) do
+          Wait(1)
+      end
+      Config.Missions[mission].Peds[i].id = CreatePed(2, Config.Missions[mission].Peds[i].modelHash, Config.Missions[mission].Peds[i].x, Config.Missions[mission].Peds[i].y, Config.Missions[mission].Peds[i].z, Config.Missions[mission].Peds[i].heading, true, true)
+      SetPedRelationshipGroupHash(Config.Missions[mission].Peds[i].id, GetHashKey("HATES_PLAYER"))
+      SetRelationshipBetweenGroups(5, GetHashKey("HATES_PLAYER"), GetHashKey("PLAYER"))
+      SetPedDropsWeaponsWhenDead(Config.Missions[mission].Peds[i].id, false)
+      SetPedDiesWhenInjured(Config.Missions[mission].Peds[i].id, true)
+      GiveWeaponToPed(Config.Missions[mission].Peds[i].id, Config.Missions[mission].Peds[i].Weapon, 2800, false, true)
+      ResetAiWeaponDamageModifier()
+      -- SetAiWeaponDamageModifier(10) 
+      -- ^ Sets Custom NPC Damage. ^
+    end
+end)
+
+RegisterCommand("mission", function(source, args, rawCommand)
+    mission = args[1]
+        if cooldown > 0 then
+            TriggerEvent('chatMessage', "^1[MISSIONS]: ^0There is a mission currently in progress.", -1)
+        else
+            TriggerEvent('chatMessage', "^1[MISSIONS]: ^2".. mission .."^0 has been launched.")
+            TriggerServerEvent('cooldownt')
+            TriggerEvent("SpawnPed", mission)
+            TriggerEvent("mt:missiontext", mission, 10000) -- 10000 = 10 Seconds.
+            TriggerEvent('missionBlips', mission)
+        end
+end, false)
 
 Citizen.CreateThread(function()
 	while true do
@@ -54,15 +110,15 @@ Citizen.CreateThread(function()
 	end
 end)
 
-	function DrawText2(text)
-        SetTextFont(0)
-        SetTextProportional(1)
-        SetTextScale(0.0, 0.45)
-        SetTextDropshadow(1, 0, 0, 0, 255)
-        SetTextEdge(1, 0, 0, 0, 255)
-        SetTextDropShadow()
-        SetTextOutline()
-        SetTextEntry("STRING")
-        AddTextComponentString(text)
-        DrawText(0.174, 0.855)
-    end
+function DrawText2(text)
+    SetTextFont(0)
+    SetTextProportional(1)
+    SetTextScale(0.0, 0.45)
+    SetTextDropshadow(1, 0, 0, 0, 255)
+    SetTextEdge(1, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(0.174, 0.855)
+end
--- missions.lua
+++ missions.lua
@@ -1,125 +1,68 @@
-local cooldown = 0
-local ispriority = false
+Config = {}
+Config.Missions = {
 
--------------------------------------------------------------------------------------
----------------------------------- MISSION ONE --------------------------------------
--------------------------------------------------------------------------------------
+  Mission1 = {
+    
+    MissionMessage = "ATTACK THE OBJECTIVE!",
 
-local Mission1Ped = {
-    -- ID: id of NPC | name: Name of Blip | BlipID: Icone of Blip | VoiceName: NPC Talk When near it | Ambiance: Ambiance of Shop | Weapon: Hash of Weapon | modelHash: Model | X: Position x | Y: Position Y | Z: Position Z | heading: Where Npc look
-    {id = 1, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -187.2, y = -1091.96, z = 27.16, heading = 27.16},
-    {id = 2, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 30.14, heading = 82.01},
-    {id = 3, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 30.14, heading = 60.94},
-    {id = 4, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 30.14, heading = 3.31448912620544},
-    {id = 5, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 30.14, heading = 3.31448912620544},
-    {id = 6, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 36.13, heading = 82.01},
-    {id = 7, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 36.13, heading = 60.94},
-    {id = 8, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 36.13, heading = 3.31448912620544},
-    {id = 9, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 36.13, heading = 3.31448912620544},
-    {id = 10, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 42.13, heading = 82.01},
-    {id = 11, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 42.13, heading = 60.94},
-    {id = 12, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 42.13, heading = 3.31448912620544},
-    {id = 13, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 42.13, heading = 3.31448912620544},
+    Blip = {
+      Title = "Objective",
+      Position = { x = -187.2, y = -1091.96, z = 27.16 },
+      Icon     = 58,
+      Display  = 4,
+      Size     = 1.2,
+      Color    = 1,
+    },
+
+    Peds = {
+        -- ID: id of NPC | name: Name of Blip | BlipID: Icone of Blip | VoiceName: NPC Talk When near it | Ambiance: Ambiance of Shop | Weapon: Hash of Weapon | modelHash: Model | X: Position x | Y: Position Y | Z: Position Z | heading: Where Npc look
+        {id = 1, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -187.2, y = -1091.96, z = 27.16, heading = 27.16},
+        {id = 2, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 30.14, heading = 82.01},
+        {id = 3, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 30.14, heading = 60.94},
+        {id = 4, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 30.14, heading = 3.31448912620544},
+        {id = 5, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 30.14, heading = 3.31448912620544},
+        {id = 6, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 36.13, heading = 82.01},
+        {id = 7, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 36.13, heading = 60.94},
+        {id = 8, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 36.13, heading = 3.31448912620544},
+        {id = 9, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 36.13, heading = 3.31448912620544},
+        {id = 10, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 42.13, heading = 82.01},
+        {id = 11, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 42.13, heading = 60.94},
+        {id = 12, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 42.13, heading = 3.31448912620544},
+        {id = 13, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 42.13, heading = 3.31448912620544},
+    },
+
+  },
+
+  -- Mission2 = {
+    
+  --   MissionMessage = "ATTACK THE OBJECTIVE!",
+
+  --   Blip = {
+  --     Title = "Objective",
+  --     Position = { x = -187.2, y = -1091.96, z = 27.16 },
+  --     Icon     = 58,
+  --     Display  = 4,
+  --     Size     = 1.2,
+  --     Color    = 1,
+  --   },
+
+  --   Peds = {
+  --       -- ID: id of NPC | name: Name of Blip | BlipID: Icone of Blip | VoiceName: NPC Talk When near it | Ambiance: Ambiance of Shop | Weapon: Hash of Weapon | modelHash: Model | X: Position x | Y: Position Y | Z: Position Z | heading: Where Npc look
+  --       {id = 1, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -187.2, y = -1091.96, z = 27.16, heading = 27.16},
+  --       {id = 2, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 30.14, heading = 82.01},
+  --       {id = 3, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 30.14, heading = 60.94},
+  --       {id = 4, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 30.14, heading = 3.31448912620544},
+  --       {id = 5, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 30.14, heading = 3.31448912620544},
+  --       {id = 6, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 36.13, heading = 82.01},
+  --       {id = 7, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 36.13, heading = 60.94},
+  --       {id = 8, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 36.13, heading = 3.31448912620544},
+  --       {id = 9, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 36.13, heading = 3.31448912620544},
+  --       {id = 10, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 42.13, heading = 82.01},
+  --       {id = 11, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 42.13, heading = 60.94},
+  --       {id = 12, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 42.13, heading = 3.31448912620544},
+  --       {id = 13, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 42.13, heading = 3.31448912620544},
+  --   },
+
+  -- },
+
 }
-
-RegisterNetEvent('Mission1SpawnPed')
-AddEventHandler('Mission1SpawnPed', function()
-    for i=1, #Mission1Ped do
-        RequestModel(GetHashKey(Mission1Ped[i].modelHash))
-    while not HasModelLoaded(GetHashKey(Mission1Ped[i].modelHash)) do
-          Wait(1)
-      end
-      Mission1Ped[i].id = CreatePed(2, Mission1Ped[i].modelHash, Mission1Ped[i].x, Mission1Ped[i].y, Mission1Ped[i].z, Mission1Ped[i].heading, true, true)
-      SetPedRelationshipGroupHash(Mission1Ped[i].id, GetHashKey("HATES_PLAYER"))
-      SetRelationshipBetweenGroups(5, GetHashKey("HATES_PLAYER"), GetHashKey("PLAYER"))
-      SetPedDropsWeaponsWhenDead(Mission1Ped[i].id, false)
-      SetPedDiesWhenInjured(Mission1Ped[i].id, true)
-      GiveWeaponToPed(Mission1Ped[i].id, Mission1Ped[i].Weapon, 2800, false, true)
-      ResetAiWeaponDamageModifier()
-      -- SetAiWeaponDamageModifier(10) 
-      -- ^ Sets Custom NPC Damage. ^
-    end
-end)
-
--------------------------------------------------------------------------------------------------
----------------------------------- Custom Mission Template --------------------------------------
--------------------------------------------------------------------------------------------------
-
--- local Mission1Ped = {
---     -- ID: id of NPC | name: Name of Blip | BlipID: Icone of Blip | VoiceName: NPC Talk When near it | Ambiance: Ambiance of Shop | Weapon: Hash of Weapon | modelHash: Model | X: Position x | Y: Position Y | Z: Position Z | heading: Where Npc look
---     {id = 1, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -187.2, y = -1091.96, z = 27.16, heading = 27.16},
---     {id = 2, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 30.14, heading = 82.01},
---     {id = 3, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 30.14, heading = 60.94},
---     {id = 4, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 30.14, heading = 3.31448912620544},
---     {id = 5, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 30.14, heading = 3.31448912620544},
---     {id = 6, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 36.13, heading = 82.01},
---     {id = 7, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 36.13, heading = 60.94},
---     {id = 8, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 36.13, heading = 3.31448912620544},
---     {id = 9, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 36.13, heading = 3.31448912620544},
---     {id = 10, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 42.13, heading = 82.01},
---     {id = 11, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 42.13, heading = 60.94},
---     {id = 12, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 42.13, heading = 3.31448912620544},
---     {id = 13, VoiceName = "GENERIC_HI", Ambiance = "AMMUCITY", Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 42.13, heading = 3.31448912620544},
--- }
-
--- RegisterNetEvent('Mission1SpawnPed')
--- AddEventHandler('Mission1SpawnPed', function()
---     for i=1, #Mission1Ped do
---         RequestModel(GetHashKey(Mission1Ped[i].modelHash))
---     while not HasModelLoaded(GetHashKey(Mission1Ped[i].modelHash)) do
---           Wait(1)
---       end
---       Mission1Ped[i].id = CreatePed(2, Mission1Ped[i].modelHash, Mission1Ped[i].x, Mission1Ped[i].y, Mission1Ped[i].z, Mission1Ped[i].heading, true, true)
---       SetPedRelationshipGroupHash(Mission1Ped[i].id, GetHashKey("HATES_PLAYER"))
---       SetRelationshipBetweenGroups(5, GetHashKey("HATES_PLAYER"), GetHashKey("PLAYER"))
---       SetPedDropsWeaponsWhenDead(Mission1Ped[i].id, false)
---       SetPedDiesWhenInjured(Mission1Ped[i].id, true)
---       GiveWeaponToPed(Mission1Ped[i].id, Mission1Ped[i].Weapon, 2800, false, true)
---       ResetAiWeaponDamageModifier()
---       -- SetAiWeaponDamageModifier(10) 
---       -- ^ Sets Custom NPC Damage. ^
---     end
--- end)
-
---------------------------------------------------------------------------------------------
----------------------------------- STARTS NEW MISSION --------------------------------------
---------------------------------------------------------------------------------------------
-
-RegisterCommand("mission", function(source, args, rawCommand)
-    print("Mission was started by: " .. ((source == 0) and 'console' or GetPlayerName(source)) )
-    mission = args[1]
-    TriggerEvent('chatMessage', "^1[MISSIONS]: ^0Mission ".. mission .." has been launched.")
-    if mission == "1" and cooldown == 0 then
-        TriggerServerEvent('cooldownt')
-        TriggerEvent("Mission1SpawnPed")
-        TriggerEvent("mt:missiontext", "~g~ Go to the Construction Site and Clear out the Resistance Forces.", 10000) -- 10000 = 10 Seconds.
-        TriggerEvent('missionBlips')
-
--- Add new option to command --
-
--- if mission == "2" then
-    -- TriggerServerEvent('cooldownt')
-    -- TriggerEvent("Mission2SpawnPed")
-    -- TriggerEvent("mt:missiontext", "~g~ (PLACE TEXT HERE)", 500)
-	elseif cooldown > 0 then
-		TriggerEvent('chatMessage', "^1[MISSIONS]: ^0There is a mission currently in progress.", -1)
-	else 
-		TriggerEvent('chatMessage', "^1[MISSIONS]: ^0This is not a mission.", -1)
-    end
-
-end, false)
-
--------------------------------------------------------------------------------------------------------
----------------------------------- KILL PEDS AFTER CANCELMISSION --------------------------------------
--------------------------------------------------------------------------------------------------------
-
-RegisterNetEvent('KP')
-AddEventHandler('KP', function()
-    for i=1, #Mission1Ped do
-    SetEntityHealth(Mission1Ped[i].id, 0)
-    end
-    -- Add new option to command --
-    -- for i=1, #Mission2Ped do
-    --     SetEntityHealth(Mission2Ped[i].id, 0)
-    -- end
-end)
