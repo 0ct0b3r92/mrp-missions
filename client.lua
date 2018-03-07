7bce8aec26c7cfbbb41b1e084fc1f2cce17aa6dc
Lance Good <lgoodyt@gmail.com>
2018-03-06 20:59:00 -0500

    Add files via upload

--- client.lua
+++ client.lua
@@ -7,16 +7,58 @@
 ------------------------------------------------------------------------------------------------
 ---------------------------------- DON'T EDIT THESE LINES --------------------------------------
 ------------------------------------------------------------------------------------------------
-local cooldown = 0
 
-local ispriority = false
+local MissionName = "N/A"
 
 Blips = {}
 
+----------------------------
+--------- Commands ---------
+----------------------------
+
+RegisterCommand("mission", function(source, args, rawCommand)
+    input = args[1]
+        for i, v in pairs(Config.Missions) do
+            if input == i then
+                if Active == 1 then
+                    TriggerEvent('chatMessage', "^1[MISSIONS]: ^2There is a mission already in progress.")
+                else
+                Active = 1
+                MissionName = input
+                TriggerEvent('chatMessage', "^1[MISSIONS]: ^2".. MissionName .."^0 has been launched.")
+                TriggerEvent("SpawnPed", input)
+                TriggerEvent("mt:missiontext", input, 10000) -- 10000 = 10 Seconds.
+                TriggerEvent('missionBlips', input)
+            end
+        end
+    end
+end, false)
+
+RegisterCommand("stop", function(source, args, rawCommand)
+    if Active == 0 then
+        TriggerEvent('chatMessage', "^1[MISSIONS]: ^2There is not a Mission in Progress.")
+    else
+        TriggerEvent('chatMessage', "^1[MISSIONS]: ^2".. MissionName .."^0 has been completed! Great work.")
+        TriggerEvent("DONE", MissionName)
+        MissionName = "N/A"
+        Active = 0
+    end
+end, false)
+
+RegisterCommand("list", function(source, rawCommand)
+    for i, v in pairs(Config.Missions) do
+        TriggerEvent('chatMessage', "^1[MISSION LIST]: ^2".. i .."")
+    end 
+end, false)
+
+--------------------------
+--------- Events ---------
+--------------------------
+
 RegisterNetEvent("mt:missiontext")
-AddEventHandler("mt:missiontext", function(mission, time)
-    for i=1, #Config.Missions[mission].MissionMessage do
-        text = Config.Missions[mission].MissionMessage
+AddEventHandler("mt:missiontext", function(input, time)
+    for i=1, #Config.Missions[input].MissionMessage do
+        text = Config.Missions[input].MissionMessage
         ClearPrints()
         SetTextEntry_2("STRING")
         AddTextComponentString(text)
@@ -24,33 +66,15 @@ AddEventHandler("mt:missiontext", function(mission, time)
     end
 end)
 
-RegisterCommand("stop", function(source, args, rawCommand)
-    mission = args[1]
-    TriggerEvent('chatMessage', "^1[MISSIONS]: ^2".. mission .."^0 has been completed! Great work.")
-    TriggerServerEvent("cancelcooldown")
-    TriggerEvent("DONE", mission)
-    TriggerEvent("DELBlips")
-end, false)
-
-RegisterNetEvent('UpdateCooldown')
-AddEventHandler('UpdateCooldown', function(newCooldown)
-    cooldown = newCooldown
-end)
-
-RegisterNetEvent('UpdateTimer')
-AddEventHandler('UpdateTimer', function(newispriority)
-    ispriority = newispriority
-end)
-
 RegisterNetEvent('missionBlips')
-AddEventHandler('missionBlips', function(mission)
-    local blip = AddBlipForCoord(Config.Missions[mission].Blip.Position.x, Config.Missions[mission].Blip.Position.y, Config.Missions[mission].Blip.Position.z)
-    SetBlipSprite (blip, Config.Missions[mission].Blip.Icon)
-    SetBlipDisplay(blip, Config.Missions[mission].Blip.Display)
-    SetBlipScale  (blip, Config.Missions[mission].Blip.Size)
-    SetBlipColour (blip, Config.Missions[mission].Blip.Color)
+AddEventHandler('missionBlips', function(input)
+    local blip = AddBlipForCoord(Config.Missions[input].Blip.Position.x, Config.Missions[input].Blip.Position.y, Config.Missions[input].Blip.Position.z)
+    SetBlipSprite (blip, Config.Missions[input].Blip.Icon)
+    SetBlipDisplay(blip, Config.Missions[input].Blip.Display)
+    SetBlipScale  (blip, Config.Missions[input].Blip.Size)
+    SetBlipColour (blip, Config.Missions[input].Blip.Color)
     SetBlipAsShortRange(blip, false)
-    local btitle = Config.Missions[mission].Blip.Title
+    local btitle = Config.Missions[input].Blip.Title
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentString(btitle)
     EndTextCommandSetBlipName(blip)
@@ -58,67 +82,45 @@ AddEventHandler('missionBlips', function(mission)
 end)
 
 RegisterNetEvent('DONE')
-AddEventHandler('DONE', function(mission)
-    for i=1, #Config.Missions[mission].Peds do
-        SetEntityHealth(Config.Missions[mission].Peds[i].id, 0)
+AddEventHandler('DONE', function(MissionName)
+    for i=1, #Config.Missions[MissionName].Peds do
+        SetEntityHealth(Config.Missions[MissionName].Peds[i].id, 0)
     end
     for i, blip in pairs(Blips) do
 		RemoveBlip(blip)
-	end
+    end
 end)
 
 RegisterNetEvent('SpawnPed')
-AddEventHandler('SpawnPed', function(mission)
-    for i=1, #Config.Missions[mission].Peds do
-        RequestModel(GetHashKey(Config.Missions[mission].Peds[i].modelHash))
-    while not HasModelLoaded(GetHashKey(Config.Missions[mission].Peds[i].modelHash)) do
+AddEventHandler('SpawnPed', function(input)
+    for i=1, #Config.Missions[input].Peds do
+        RequestModel(GetHashKey(Config.Missions[input].Peds[i].modelHash))
+    while not HasModelLoaded(GetHashKey(Config.Missions[input].Peds[i].modelHash)) do
           Wait(1)
       end
-      Config.Missions[mission].Peds[i].id = CreatePed(2, Config.Missions[mission].Peds[i].modelHash, Config.Missions[mission].Peds[i].x, Config.Missions[mission].Peds[i].y, Config.Missions[mission].Peds[i].z, Config.Missions[mission].Peds[i].heading, true, true)
-      SetPedRelationshipGroupHash(Config.Missions[mission].Peds[i].id, GetHashKey("HATES_PLAYER"))
+      Config.Missions[input].Peds[i].id = CreatePed(2, Config.Missions[input].Peds[i].modelHash, Config.Missions[input].Peds[i].x, Config.Missions[input].Peds[i].y, Config.Missions[input].Peds[i].z, Config.Missions[input].Peds[i].heading, true, true)
+      SetPedRelationshipGroupHash(Config.Missions[input].Peds[i].id, GetHashKey("HATES_PLAYER"))
       SetRelationshipBetweenGroups(5, GetHashKey("HATES_PLAYER"), GetHashKey("PLAYER"))
-      SetPedDropsWeaponsWhenDead(Config.Missions[mission].Peds[i].id, false)
-      SetPedDiesWhenInjured(Config.Missions[mission].Peds[i].id, true)
-      GiveWeaponToPed(Config.Missions[mission].Peds[i].id, Config.Missions[mission].Peds[i].Weapon, 2800, false, true)
+      SetPedDropsWeaponsWhenDead(Config.Missions[input].Peds[i].id, false)
+      SetPedDiesWhenInjured(Config.Missions[input].Peds[i].id, true)
+      GiveWeaponToPed(Config.Missions[input].Peds[i].id, Config.Missions[input].Peds[i].Weapon, 2800, false, true)
       ResetAiWeaponDamageModifier()
       -- SetAiWeaponDamageModifier(10) 
       -- ^ Sets Custom NPC Damage. ^
     end
 end)
 
-RegisterCommand("mission", function(source, args, rawCommand)
-    mission = args[1]
-        if cooldown > 0 then
-            TriggerEvent('chatMessage', "^1[MISSIONS]: ^0There is a mission currently in progress.", -1)
-        else
-            TriggerEvent('chatMessage', "^1[MISSIONS]: ^2".. mission .."^0 has been launched.")
-            TriggerServerEvent('cooldownt')
-            TriggerEvent("SpawnPed", mission)
-            TriggerEvent("mt:missiontext", mission, 10000) -- 10000 = 10 Seconds.
-            TriggerEvent('missionBlips', mission)
-        end
-end, false)
+-----------------------------
+--------- Functions ---------
+-----------------------------
 
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-        if cooldown == 0 then
-            DrawText2("")
-        elseif ispriority == false then
-            DrawText2("Mission Timer: ~r~".. cooldown .." ~w~Mins")
-        end
-	end
-end)
+--------------------
+--Mission Debugger--
+--------------------
 
-function DrawText2(text)
-    SetTextFont(0)
-    SetTextProportional(1)
-    SetTextScale(0.0, 0.45)
-    SetTextDropshadow(1, 0, 0, 0, 255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextDropShadow()
-    SetTextOutline()
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(0.174, 0.855)
-end
+-- Citizen.CreateThread(function()
+--     while true do
+--         Citizen.Wait(5000)
+--         TriggerEvent('chatMessage', ""..MissionName.."")
+--     end
+-- end)
--- missions.lua
+++ missions.lua
@@ -33,36 +33,36 @@ Config.Missions = {
 
   },
 
-  -- Mission2 = {
+  Mission2 = {
     
-  --   MissionMessage = "ATTACK THE OBJECTIVE!",
+    MissionMessage = "ATTACK THE OBJECTIVE!",
 
-  --   Blip = {
-  --     Title = "Objective",
-  --     Position = { x = -187.2, y = -1091.96, z = 27.16 },
-  --     Icon     = 58,
-  --     Display  = 4,
-  --     Size     = 1.2,
-  --     Color    = 1,
-  --   },
+    Blip = {
+      Title = "Objective",
+      Position = { x = -187.2, y = -1091.96, z = 27.16 },
+      Icon     = 58,
+      Display  = 4,
+      Size     = 1.2,
+      Color    = 1,
+    },
 
-  --   Peds = {
-  --       -- ID: id of NPC | name: Name of Blip | BlipID: Icone of Blip | VoiceName: NPC Talk When near it | Ambiance: Ambiance of Shop | Weapon: Hash of Weapon | modelHash: Model | X: Position x | Y: Position Y | Z: Position Z | heading: Where Npc look
-  --       {id = 1, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -187.2, y = -1091.96, z = 27.16, heading = 27.16},
-  --       {id = 2, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 30.14, heading = 82.01},
-  --       {id = 3, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 30.14, heading = 60.94},
-  --       {id = 4, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 30.14, heading = 3.31448912620544},
-  --       {id = 5, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 30.14, heading = 3.31448912620544},
-  --       {id = 6, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 36.13, heading = 82.01},
-  --       {id = 7, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 36.13, heading = 60.94},
-  --       {id = 8, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 36.13, heading = 3.31448912620544},
-  --       {id = 9, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 36.13, heading = 3.31448912620544},
-  --       {id = 10, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 42.13, heading = 82.01},
-  --       {id = 11, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 42.13, heading = 60.94},
-  --       {id = 12, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 42.13, heading = 3.31448912620544},
-  --       {id = 13, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 42.13, heading = 3.31448912620544},
-  --   },
+    Peds = {
+        -- ID: id of NPC | name: Name of Blip | BlipID: Icone of Blip | VoiceName: NPC Talk When near it | Ambiance: Ambiance of Shop | Weapon: Hash of Weapon | modelHash: Model | X: Position x | Y: Position Y | Z: Position Z | heading: Where Npc look
+        {id = 1, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -187.2, y = -1091.96, z = 27.16, heading = 27.16},
+        {id = 2, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 30.14, heading = 82.01},
+        {id = 3, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 30.14, heading = 60.94},
+        {id = 4, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 30.14, heading = 3.31448912620544},
+        {id = 5, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 30.14, heading = 3.31448912620544},
+        {id = 6, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 36.13, heading = 82.01},
+        {id = 7, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 36.13, heading = 60.94},
+        {id = 8, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 36.13, heading = 3.31448912620544},
+        {id = 9, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 36.13, heading = 3.31448912620544},
+        {id = 10, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -179.47, y = -1078.39, z = 42.13, heading = 82.01},
+        {id = 11, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -173.8, y = -1062.78, z = 42.13, heading = 60.94},
+        {id = 12, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -160.2, y = -1067.93, z = 42.13, heading = 3.31448912620544},
+        {id = 13, Weapon = 0x83BF0278, modelHash = "s_m_y_ammucity_01", x = -165.83, y = -1083.26, z = 42.13, heading = 3.31448912620544},
+    },
 
-  -- },
+  },
 
 }
